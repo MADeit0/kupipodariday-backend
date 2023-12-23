6941816638edee29e310a6cee0fe8746a749ea97
Олег Цыпуштанов <madeit@MacBook-Pro-Oleg.local>
2023-12-24 00:41:50 +0300

    feat: create a schema for wishes

--- src/wishes/entities/wish.entity.ts
+++ src/wishes/entities/wish.entity.ts
@@ -1,1 +1,35 @@
-export class Wish {}
+import { IsInt, IsNumber, IsString, IsUrl, Length } from 'class-validator';
+import { BaseEntity } from 'src/entities/base.entity';
+import { Column } from 'typeorm';
+
+export class Wish extends BaseEntity {
+  @Column({ length: 250 })
+  @IsString()
+  @Length(1, 250)
+  name: string;
+
+  @Column()
+  @IsUrl()
+  link: string;
+
+  @Column()
+  @IsUrl()
+  image: string;
+
+  @Column('decimal', { precision: 5, scale: 2, default: 0 })
+  @IsNumber({ maxDecimalPlaces: 2 })
+  price: number;
+
+  @Column('decimal', { precision: 5, scale: 2, default: 0 })
+  @IsNumber({ maxDecimalPlaces: 2 })
+  raised: number;
+
+  @Column({ length: 1024 })
+  @IsString()
+  @Length(1, 1024)
+  description: string;
+
+  @Column('int', { default: 0 })
+  @IsInt()
+  copied: number;
+}
