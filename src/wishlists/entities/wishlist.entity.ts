b5cfac21e74254b4228879ed1cb252a8a3a51109
Олег Цыпуштанов <madeit@MacBook-Pro-Oleg.local>
2023-12-24 11:40:30 +0300

    feat: create a schema for wishlists

--- src/wishlists/entities/wishlist.entity.ts
+++ src/wishlists/entities/wishlist.entity.ts
@@ -1,1 +1,19 @@
-export class Wishlist {}
+import { IsString, IsUrl, Length } from 'class-validator';
+import { BaseEntity } from 'src/entities/base.entity';
+import { Column } from 'typeorm';
+
+export class Wishlist extends BaseEntity {
+  @Column({ length: 250 })
+  @IsString()
+  @Length(1, 250)
+  name: string;
+
+  @Column({ length: 1500 })
+  @IsString()
+  @Length(1, 1500)
+  description: string;
+
+  @Column()
+  @IsUrl()
+  image: string;
+}
