6e9625879db6cf9aac507d5c438a79a9aa32a0a3
Олег Цыпуштанов <madeit@MacBook-Pro-Oleg.local>
2024-01-03 16:58:05 +0300

    feat: transferring configuration to .env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,18 +8,24 @@ import { Wish } from './wishes/entities/wish.entity';
 import { Wishlist } from './wishlists/entities/wishlist.entity';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'student',
-      password: 'student',
-      database: 'kupipodariday',
-      entities: [User, Offer, Wish, Wishlist],
-      synchronize: true,
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('PG_HOST'),
+        port: +configService.get('PR_PORT'),
+        username: configService.get('PR_USERNAME'),
+        password: configService.get('PG_PASSWORD'),
+        database: configService.get('PG_DB'),
+        synchronize: configService.get('SYNCHRONIZE'),
+        entities: [User, Offer, Wish, Wishlist],
+      }),
     }),
     UsersModule,
     AuthModule,
