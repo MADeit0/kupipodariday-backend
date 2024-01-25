1dc9ee2881bf65db6d7352451e85ee94a4b5e154
Олег Цыпуштанов <madeit@MacBook-Pro-Oleg.local>
2024-01-25 20:12:50 +0300

    refact: make the entire wishes controller secure

--- src/wishes/wishes.controller.ts
+++ src/wishes/wishes.controller.ts
@@ -6,8 +6,6 @@ import {
   Request,
   UseGuards,
   Param,
-  UseInterceptors,
-  ClassSerializerInterceptor,
   Patch,
   Delete,
 } from '@nestjs/common';
@@ -15,10 +13,8 @@ import { WishesService } from './wishes.service';
 import { CreateWishDto } from './dto/create-wish.dto';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { User } from 'src/users/entities/user.entity';
-import { RemoveEmailInterceptor } from 'src/users/interseptors/remove-email.interceptor';
 import { UpdateWishDto } from './dto/update-wish.dto';
 
-@UseInterceptors(ClassSerializerInterceptor)
 @UseGuards(JwtAuthGuard)
 @Controller('wishes')
 export class WishesController {
@@ -39,13 +35,11 @@ export class WishesController {
     return this.wishesService.findLastWishes();
   }
 
-  @UseInterceptors(RemoveEmailInterceptor)
   @Get(':id')
   getWishesById(@Param('id') id: number) {
     return this.wishesService.findOne(id);
   }
 
-  @UseGuards(JwtAuthGuard)
   @Patch(':id')
   updateWish(
     @Request() req: { user: User },
@@ -55,13 +49,11 @@ export class WishesController {
     return this.wishesService.updateWish(id, updateWishDto, req.user.id);
   }
 
-  @UseGuards(JwtAuthGuard)
   @Delete(':id')
   removeWish(@Request() req: { user: User }, @Param('id') id: number) {
     return this.wishesService.removeOne(id, req.user.id);
   }
 
-  @UseGuards(JwtAuthGuard)
   @Post(':id/copy')
   copyWish(@Param('id') id: number, @Request() req: { user: User }) {
     return this.wishesService.copy(id, req.user.id);
