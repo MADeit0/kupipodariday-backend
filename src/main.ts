ce252249bac1b44a62473978e1809b0161678485
Олег Цыпуштанов <madeit@MacBook-Pro-Oleg.local>
2024-01-27 11:42:02 +0300

    feat: install helmet and cookie-parser

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,17 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { useContainer } from 'class-validator';
 import { initializeTransactionalContext } from 'typeorm-transactional';
+import helmet from 'helmet';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   initializeTransactionalContext();
-
   const app = await NestFactory.create(AppModule, {
     cors: true,
     abortOnError: true,
   });
+  app.use(helmet());
+  app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
   await app.listen(3001);
